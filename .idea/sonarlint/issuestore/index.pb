f6751d0a7bda89173342971b0f2e2c1ca17be4e2
energyaudit <energyaudit668@gmail.com>
2020-10-10 21:50:41 -0400

    [billy] initial commit

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,15 @@
+
+9
+	README.md,8\e\8ec9a00bfd09b3190ac6b22251dbb1aa95a0579d
+n
+>src/test/java/com/factory/cucumber/runner/RunCucumberTest.java,9\c\9c6c22188be92806ec6f4afd7c15e5d6317ba850
+r
+Bsrc/test/java/com/factory/cucumber/features/DemoFeatureGet.feature,0\0\00cf9b064f71abb4cc996a295ef883780caac577
+s
+Csrc/test/java/com/factory/cucumber/features/DemoFeaturePost.feature,e\0\e087c69b1cd46147930d477f10670d126c1f3615
+r
+Bsrc/test/java/com/factory/cucumber/features/DemoFeaturePut.feature,5\9\59cfcd48edc4729c0d18eff43a7fa4be8cb60269
+i
+9src/test/java/com/factory/cucumber/features/Hooks.feature,3\4\344190b05b00fb831126dd4be6c9cc7fcca7151f
+l
+<src/test/java/com/factory/cucumber/features/TagsDemo.feature,b\2\b29d2b3151e689b568e641e5bf9fc43e1a873b74
---
+++ output/html-report/report.js
@@ -0,0 +1,577 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("DemoFeatureGet.feature");
+formatter.feature({
+  "line": 2,
+  "name": "GET: Automated Demo Tests",
+  "description": "Description: GET: purpose of this feature is to test some demo app.",
+  "id": "get:-automated-demo-tests",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 6,
+  "name": "GET: Test the Demo app",
+  "description": "",
+  "id": "get:-automated-demo-tests;get:-test-the-demo-app",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 5,
+      "name": "@get"
+    }
+  ]
+});
+formatter.step({
+  "line": 7,
+  "name": "I want to set URL as \"\u003cURL\u003e\" for test case \"\u003cTestName\u003e\"",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "I set header content type as \"\u003cContentType\u003e\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "I hit the API with requestbody \"\u003cRequestBody\u003e\" and request method is \"\u003cRequestMethod\u003e\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "I try to verify the status code is \"\u003cStatusCode\u003e\"",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 12,
+  "name": "",
+  "description": "",
+  "id": "get:-automated-demo-tests;get:-test-the-demo-app;",
+  "rows": [
+    {
+      "cells": [
+        "TestName",
+        "URL",
+        "ContentType",
+        "RequestBody",
+        "RequestMethod",
+        "StatusCode"
+      ],
+      "line": 13,
+      "id": "get:-automated-demo-tests;get:-test-the-demo-app;;1"
+    },
+    {
+      "cells": [
+        "Demo test",
+        "/api/users?page\u003d2",
+        "application/json",
+        "",
+        "GET",
+        "200"
+      ],
+      "line": 14,
+      "id": "get:-automated-demo-tests;get:-test-the-demo-app;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.before({
+  "duration": 23880682,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 434174,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 1074108,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 14,
+  "name": "GET: Test the Demo app",
+  "description": "",
+  "id": "get:-automated-demo-tests;get:-test-the-demo-app;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 5,
+      "name": "@get"
+    }
+  ]
+});
+formatter.step({
+  "line": 7,
+  "name": "I want to set URL as \"/api/users?page\u003d2\" for test case \"Demo test\"",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "I set header content type as \"application/json\"",
+  "matchedColumns": [
+    2
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "I hit the API with requestbody \"\" and request method is \"GET\"",
+  "matchedColumns": [
+    3,
+    4
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "I try to verify the status code is \"200\"",
+  "matchedColumns": [
+    5
+  ],
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "/api/users?page\u003d2",
+      "offset": 22
+    },
+    {
+      "val": "Demo test",
+      "offset": 56
+    }
+  ],
+  "location": "StepDefinitionsTest.setAPIEndpointURL(String,String)"
+});
+formatter.result({
+  "duration": 148506632,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "application/json",
+      "offset": 30
+    }
+  ],
+  "location": "StepDefinitionsTest.setHeader(String)"
+});
+formatter.result({
+  "duration": 1526026,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "",
+      "offset": 32
+    },
+    {
+      "val": "GET",
+      "offset": 57
+    }
+  ],
+  "location": "StepDefinitionsTest.submitRequest(String,String)"
+});
+formatter.result({
+  "duration": 2121762670,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "200",
+      "offset": 36
+    }
+  ],
+  "location": "StepDefinitionsTest.verifyStatusCode(String)"
+});
+formatter.result({
+  "duration": 2316599,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 1097515,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 1135647,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 1745756,
+  "status": "passed"
+});
+formatter.uri("Hooks.feature");
+formatter.feature({
+  "line": 2,
+  "name": "GET: Hooks Demo",
+  "description": "Description: GET: purpose of this feature is to test demo Hooks.",
+  "id": "get:-hooks-demo",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 6,
+  "name": "GET: Test Hooks",
+  "description": "",
+  "id": "get:-hooks-demo;get:-test-hooks",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 5,
+      "name": "@hooks"
+    }
+  ]
+});
+formatter.step({
+  "comments": [
+    {
+      "line": 7,
+      "value": "#Given I want to set URL as \"\u003cURL\u003e\" for test case \"\u003cTestName\u003e\""
+    }
+  ],
+  "line": 8,
+  "name": "I set header content type as \"\u003cContentType\u003e\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "I hit the API with requestbody \"\u003cRequestBody\u003e\" and request method is \"\u003cRequestMethod\u003e\"",
+  "keyword": "When "
+});
+formatter.examples({
+  "comments": [
+    {
+      "line": 10,
+      "value": "#Then I try to verify the status code is \"\u003cStatusCode\u003e\""
+    }
+  ],
+  "line": 12,
+  "name": "",
+  "description": "",
+  "id": "get:-hooks-demo;get:-test-hooks;",
+  "rows": [
+    {
+      "cells": [
+        "ContentType",
+        "RequestBody",
+        "RequestMethod"
+      ],
+      "line": 13,
+      "id": "get:-hooks-demo;get:-test-hooks;;1"
+    },
+    {
+      "cells": [
+        "application/json",
+        "",
+        "GET"
+      ],
+      "line": 14,
+      "id": "get:-hooks-demo;get:-test-hooks;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.before({
+  "duration": 386981,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 319023,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 2036463,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 14,
+  "name": "GET: Test Hooks",
+  "description": "",
+  "id": "get:-hooks-demo;get:-test-hooks;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 5,
+      "name": "@hooks"
+    }
+  ]
+});
+formatter.step({
+  "comments": [
+    {
+      "line": 7,
+      "value": "#Given I want to set URL as \"\u003cURL\u003e\" for test case \"\u003cTestName\u003e\""
+    }
+  ],
+  "line": 8,
+  "name": "I set header content type as \"application/json\"",
+  "matchedColumns": [
+    0
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "I hit the API with requestbody \"\" and request method is \"GET\"",
+  "matchedColumns": [
+    1,
+    2
+  ],
+  "keyword": "When "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "application/json",
+      "offset": 30
+    }
+  ],
+  "location": "StepDefinitionsTest.setHeader(String)"
+});
+formatter.result({
+  "duration": 593496,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "",
+      "offset": 32
+    },
+    {
+      "val": "GET",
+      "offset": 57
+    }
+  ],
+  "location": "StepDefinitionsTest.submitRequest(String,String)"
+});
+formatter.result({
+  "duration": 139568319,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 587078,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 274851,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 289953,
+  "status": "passed"
+});
+formatter.uri("TagsDemo.feature");
+formatter.feature({
+  "line": 2,
+  "name": "GET: Tags Demo Tests",
+  "description": "Description: GET: purpose of this feature is to test Tags demo app.",
+  "id": "get:-tags-demo-tests",
+  "keyword": "Feature"
+});
+formatter.scenarioOutline({
+  "line": 6,
+  "name": "GET: Test the Tags Demo app",
+  "description": "",
+  "id": "get:-tags-demo-tests;get:-test-the-tags-demo-app",
+  "type": "scenario_outline",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 5,
+      "name": "@smokes"
+    }
+  ]
+});
+formatter.step({
+  "line": 7,
+  "name": "I want to set URL as \"\u003cURL\u003e\" for test case \"\u003cTestName\u003e\"",
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "I set header content type as \"\u003cContentType\u003e\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "I hit the API with requestbody \"\u003cRequestBody\u003e\" and request method is \"\u003cRequestMethod\u003e\"",
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "I try to verify the status code is \"\u003cStatusCode\u003e\"",
+  "keyword": "Then "
+});
+formatter.examples({
+  "line": 12,
+  "name": "",
+  "description": "",
+  "id": "get:-tags-demo-tests;get:-test-the-tags-demo-app;",
+  "rows": [
+    {
+      "cells": [
+        "TestName",
+        "URL",
+        "ContentType",
+        "RequestBody",
+        "RequestMethod",
+        "StatusCode"
+      ],
+      "line": 13,
+      "id": "get:-tags-demo-tests;get:-test-the-tags-demo-app;;1"
+    },
+    {
+      "cells": [
+        "Demo test",
+        "/api/users?page\u003d2",
+        "application/json",
+        "",
+        "GET",
+        "200"
+      ],
+      "line": 14,
+      "id": "get:-tags-demo-tests;get:-test-the-tags-demo-app;;2"
+    }
+  ],
+  "keyword": "Examples"
+});
+formatter.before({
+  "duration": 456071,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 312982,
+  "status": "passed"
+});
+formatter.before({
+  "duration": 776604,
+  "status": "passed"
+});
+formatter.scenario({
+  "line": 14,
+  "name": "GET: Test the Tags Demo app",
+  "description": "",
+  "id": "get:-tags-demo-tests;get:-test-the-tags-demo-app;;2",
+  "type": "scenario",
+  "keyword": "Scenario Outline",
+  "tags": [
+    {
+      "line": 5,
+      "name": "@smokes"
+    }
+  ]
+});
+formatter.step({
+  "line": 7,
+  "name": "I want to set URL as \"/api/users?page\u003d2\" for test case \"Demo test\"",
+  "matchedColumns": [
+    0,
+    1
+  ],
+  "keyword": "Given "
+});
+formatter.step({
+  "line": 8,
+  "name": "I set header content type as \"application/json\"",
+  "matchedColumns": [
+    2
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 9,
+  "name": "I hit the API with requestbody \"\" and request method is \"GET\"",
+  "matchedColumns": [
+    3,
+    4
+  ],
+  "keyword": "When "
+});
+formatter.step({
+  "line": 10,
+  "name": "I try to verify the status code is \"200\"",
+  "matchedColumns": [
+    5
+  ],
+  "keyword": "Then "
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "/api/users?page\u003d2",
+      "offset": 22
+    },
+    {
+      "val": "Demo test",
+      "offset": 56
+    }
+  ],
+  "location": "StepDefinitionsTest.setAPIEndpointURL(String,String)"
+});
+formatter.result({
+  "duration": 4784594,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "application/json",
+      "offset": 30
+    }
+  ],
+  "location": "StepDefinitionsTest.setHeader(String)"
+});
+formatter.result({
+  "duration": 589344,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "",
+      "offset": 32
+    },
+    {
+      "val": "GET",
+      "offset": 57
+    }
+  ],
+  "location": "StepDefinitionsTest.submitRequest(String,String)"
+});
+formatter.result({
+  "duration": 182741410,
+  "status": "passed"
+});
+formatter.match({
+  "arguments": [
+    {
+      "val": "200",
+      "offset": 36
+    }
+  ],
+  "location": "StepDefinitionsTest.verifyStatusCode(String)"
+});
+formatter.result({
+  "duration": 596895,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 530069,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 234832,
+  "status": "passed"
+});
+formatter.after({
+  "duration": 260127,
+  "status": "passed"
+});
+});
